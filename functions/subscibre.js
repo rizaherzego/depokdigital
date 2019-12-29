e039699c52a8b971e0d2829b8660a4fd7efa010c
Riza Herzego <herzego@outlook.com>
2019-12-29 23:53:38 +0700

    try send json response

--- functions/subscibre.js
+++ functions/subscibre.js
@@ -8,11 +8,15 @@ exports.handler = async (event, context) => {
 
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
-  const params = querystring.parse(event.body);
-  const name = params.name || "World";
+  //   const params = querystring.parse(event.body);
+  //   const name = params.name || "World";
 
+  
+  console.log('hello ' + event.body.email);
+  
   return {
     statusCode: 200,
-    body: `Hello, ${name}`
+    email:event.body.email,
+    full_name:event.body.full_name
   };
 };
