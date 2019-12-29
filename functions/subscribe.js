4e2a169429364dcca0180b649526d7fb14763271
Riza Herzego <herzego@outlook.com>
2019-12-29 23:56:41 +0700

    try send json response

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -8,15 +8,14 @@ exports.handler = async (event, context) => {
 
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
-  //   const params = querystring.parse(event.body);
-  //   const name = params.name || "World";
+  const params = querystring.parse(event.body);
+  const email = params.email || "World";
 
   
   console.log('hello ' + event.body.email);
   
   return {
     statusCode: 200,
-    email:event.body.email,
-    full_name:event.body.full_name
+    email:email
   };
 };
